2d97729c3dead5f11bbe13d6e2b2df589c670b0c
Luke Wright <luke@simplyluke.com>
2014-11-11 14:11:21 -0600

    comment out all references to teams thanks page, effectively removing it from production

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  def after_sign_up_path_for(resource)
-    '/registration/thanks'
-  end
+  # def after_sign_up_path_for(resource)
+  #   '/registration/thanks'
+  # end
 end
