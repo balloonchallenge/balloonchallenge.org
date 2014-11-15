19e7ad40d3064e6d726c10b6b0a27e97637f693f
Krish Dholakiya <krishna.dholakiya@gmail.com>
2014-11-15 09:49:45 -0700

    sweetAlert > alert

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -10,9 +10,10 @@ function init(){
         .forEach(function(link){
                 link.addEventListener('click', function(e) {
                         e.preventDefault();
-                        alert('Looks like this isn\'t ready yet, but it\'ll be up soon. Stay tuned!');
+                        sweetAlert('Looks like this isn\'t ready yet, but it\'ll be up soon. Stay tuned!');
                 });
         });
+    swal({   title: "Error!",   text: "Here's my error message!",   type: "error",   confirmButtonText: "Cool" });
     (function lazyLoad() {
         $(".gallery img").unveil();
         document.querySelectorAll(".gallery img")
