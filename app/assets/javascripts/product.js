c4fa33a79ddb6d01127c182aae286f462903644b
Krish Dholakiya <krishna.dholakiya@gmail.com>
2014-11-15 11:56:10 -0700

    gallery done

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -8,18 +8,16 @@ function init(){
         if($('.image')) {
             $('.slideshow').slick();
         }
+        $('.fancybox').fancybox();
     })();
     (function notReady() {
         NodeList.prototype.forEach = Array.prototype.forEach;
         document.querySelectorAll('a[href="#"]')
             .forEach(function(link){
                     link.addEventListener('click', function(e) {
-                            console.log('yo');
                             e.preventDefault();
                             swal('Looks like this isn\'t ready yet, but it\'ll be up soon. Stay tuned!');
                     });
-                    console.log('Added event listener to:');
-                    console.log(link.innerHTML);
             });
     })();
     (function lazyLoad() {
@@ -27,7 +25,7 @@ function init(){
         document.querySelectorAll(".gallery img")
             .forEach(function(img) {
                     img.addEventListener('click', function(e) {
-                            window.location = this.src;
+                            //window.location = this.src;
                     });
             });
     })();
