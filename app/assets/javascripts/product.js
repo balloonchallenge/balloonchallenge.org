18f58a38f4ffae8a5e19b7d3d826780e17bb466e
Krish Dholakiya <krishna.dholakiya@gmail.com>
2014-11-15 10:20:54 -0700

    :metal: hell yes

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -3,17 +3,23 @@ $(document).on('page:change', init);
 $(document).ready(init);
 
 function init(){
-    if($('.image')) {
-        $('.slideshow').slick();
-    }
+    initSweet();
+    (function slideshow() {
+        if($('.image')) {
+            $('.slideshow').slick();
+        }
+    })();
     (function notReady() {
         NodeList.prototype.forEach = Array.prototype.forEach;
         document.querySelectorAll('a[href="#"]')
             .forEach(function(link){
                     link.addEventListener('click', function(e) {
+                            console.log('yo');
                             e.preventDefault();
-                            sweetAlert('Looks like this isn\'t ready yet, but it\'ll be up soon. Stay tuned!');
+                            swal('Looks like this isn\'t ready yet, but it\'ll be up soon. Stay tuned!');
                     });
+                    console.log('Added event listener to:');
+                    console.log(link.innerHTML);
             });
     })();
     (function lazyLoad() {
