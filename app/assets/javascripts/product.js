b1a8a9270997b9e19dc22ed929672c37c6fad43a
Krish Dholakiya <krishna.dholakiya@gmail.com>
2014-11-15 10:04:29 -0700

    should fix some conflicts between turblolinks and the map and placeholder alert

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -1,19 +1,21 @@
 $(document).on('page:load', init);
+$(document).on('page:change', init);
 $(document).ready(init);
 
 function init(){
     if($('.image')) {
         $('.slideshow').slick();
     }
-    NodeList.prototype.forEach = Array.prototype.forEach;
-    document.querySelectorAll('a[href="#"]')
-        .forEach(function(link){
-                link.addEventListener('click', function(e) {
-                        e.preventDefault();
-                        sweetAlert('Looks like this isn\'t ready yet, but it\'ll be up soon. Stay tuned!');
-                });
-        });
-    swal({   title: "Error!",   text: "Here's my error message!",   type: "error",   confirmButtonText: "Cool" });
+    (function notReady() {
+        NodeList.prototype.forEach = Array.prototype.forEach;
+        document.querySelectorAll('a[href="#"]')
+            .forEach(function(link){
+                    link.addEventListener('click', function(e) {
+                            e.preventDefault();
+                            sweetAlert('Looks like this isn\'t ready yet, but it\'ll be up soon. Stay tuned!');
+                    });
+            });
+    })();
     (function lazyLoad() {
         $(".gallery img").unveil();
         document.querySelectorAll(".gallery img")
