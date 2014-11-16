abfe8a793f94f485a3efd43eca02ee0131144491
Krish Dholakiya <krishna.dholakiya@gmail.com>
2014-11-15 18:36:19 -0700

    added tracker products

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
   match '/supplies',  to: 'static_pages#supplies',  via: 'get'
   match '/mission-ops',to:'static_pages#ops',       via: 'get'
   match '/tracker',   to: 'static_pages#tracker',   via: 'get'
+  match '/tracker-products',to:'static_pages#tracker-products',via:'get'
   # match '/registration/thanks', to: 'static_pages#thanks_register', via: 'get'
   match '/teams/:id/add_member/', to: 'teams#add_member', via: 'post', as: 'add_member'
   match '/teams/:id/remove_member/', to: 'teams#remove_member', via: 'delete', as: 'remove_member'
