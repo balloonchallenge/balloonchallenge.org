c3eaf3012c4493dc08ab0de467d18cf0c742fda0
Luke Wright <luke@simplyluke.com>
2014-12-07 16:21:49 -0600

    fix my migration for postgres

--- db/migrate/20141207221714_fix_postgres.rb
+++ db/migrate/20141207221714_fix_postgres.rb
@@ -1,5 +1,5 @@
 class FixPostgres < ActiveRecord::Migration
   def change
-    change_column :requests, :user_id, :integer
+    change_column :requests, :user_id, 'integer USING CAST(user_id AS integer)'
   end
 end
