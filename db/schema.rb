821383f0c1cdd34c2d68fddfce52776203615502
Luke Wright <luke@simplyluke.com>
2014-12-07 16:19:38 -0600

    fix postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207180913) do
+ActiveRecord::Schema.define(version: 20141207221714) do
 
   create_table "requests", force: true do |t|
-    t.string   "user_id"
+    t.integer  "user_id",    limit: 255
     t.integer  "team_id"
     t.datetime "created_at"
     t.datetime "updated_at"
