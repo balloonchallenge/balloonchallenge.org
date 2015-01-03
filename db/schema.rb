5e19304dc879991ef4eae5864d38c9fc396a1f2b
Luke Wright <luke@simplyluke.com>
2015-01-03 17:07:18 -0600

    make users confirm their account

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207221714) do
+ActiveRecord::Schema.define(version: 20150103230501) do
 
   create_table "requests", force: true do |t|
     t.integer  "user_id",    limit: 255
@@ -68,8 +68,12 @@ ActiveRecord::Schema.define(version: 20141207221714) do
     t.boolean  "gsbc_last_year"
     t.string   "first_name",             default: ""
     t.string   "last_name",              default: ""
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["team_id"], name: "index_users_on_team_id"
